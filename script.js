0896ad7a6a6d5c5d2dbf039fbdf9102aef404dc1
Jorge Valderrabano <99094593+JorgeValderrabano@users.noreply.github.com>
2024-02-27 15:22:37 -0600

    Update script.js
    Updated version 1.70

--- script.js
+++ script.js
@@ -270,7 +270,10 @@ function pick(guess) {
     numbers.push(Math.floor(Math.random() * 11));
   }
   text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
-  for (let x = 1; x < 5; x++) {
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
 
   }
 }
